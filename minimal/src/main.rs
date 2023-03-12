18e65e96d550d84d9710bb74d5cfaf58e4ae8d1b
David D <suchmonkey@protonmail.com>
2023-03-12 12:30:49 +0100

    fix enum

--- minimal/src/main.rs
+++ minimal/src/main.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(Msaa::Sample8)
         .add_plugins(DefaultPlugins)
         .add_system(bevy::window::close_on_esc)
         .run();
